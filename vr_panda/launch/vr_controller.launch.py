eca82f5292dfdccf61998dc0af8e82f569553219
09ubberboy90 <2330834a@student.gla.ac.uk>
2022-03-28 14:48:51 +0100

    Current

--- vr_panda/launch/vr_controller.launch.py
+++ vr_panda/launch/vr_controller.launch.py
@@ -3,7 +3,6 @@ import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import xacro
 
 def load_file(package_name, file_path):
     package_path = get_package_share_directory(package_name)
@@ -29,29 +28,24 @@ def load_yaml(package_name, file_path):
 
 def generate_launch_description():
     # planning_context
-    robot_description_config = load_file("ur_description", 
+    robot_description_config = load_file("vr_panda", 
         os.path.join(
             "urdf",
-            "ur.urdf",
+            "panda.urdf",
         )
-)
+    )
     robot_description = {"robot_description": robot_description_config}
 
     robot_description_semantic_config = load_file(
-        "ur_moveit_config", "srdf/ur.srdf"
+        "vr_panda", "config/panda.srdf"
     )
     robot_description_semantic = {
         "robot_description_semantic": robot_description_semantic_config
     }
 
     kinematics_yaml = load_yaml(
-        "ur_description", "config/ur3/default_kinematics.yaml"
+        "moveit_resources_panda_moveit_config", "config/kinematics.yaml"
     )
-    servo_yaml = load_yaml(
-        "vr_panda", "config/ur_simulated.yaml"
-    )
-    servo_params = {"moveit_servo": servo_yaml}
-
     # MoveGroupInterface demo executable
     moveit_controller = Node(name='moveit_controller',
                                package='vr_controller',
@@ -59,7 +53,7 @@ def generate_launch_description():
                                output='screen',
                                parameters=[robot_description,
                                            robot_description_semantic,
-                                           servo_params,
+                                           kinematics_yaml,
                                            ],
                             #   prefix=['gdbserver localhost:3000']
                             )
