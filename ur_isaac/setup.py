b3dfcaff2edd24901b2fd8d2479bb0b074165ddf
09ubberboy90 <2330834a@student.gla.ac.uk>
2023-02-14 14:52:23 +0000

    Add more config file to build

--- ur_isaac/setup.py
+++ ur_isaac/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'ur_isaac'
 
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdfs', glob('urdfs/*.urdf')),
+        ('share/' + package_name + '/config', glob('config/*.*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
